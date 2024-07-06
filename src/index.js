c97b67544b5bafce39ef9210fda26866794bf46f
unknown <abdelrahmanmustafa088@gmail.com>
2024-07-06 15:47:01 +0200

    new

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RoomProvider } from "./context";
 
-import 'react-toastify/dist/ReactToastify.css'; 
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RoomProvider>
